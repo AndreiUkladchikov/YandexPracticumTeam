55e4da80c5eb90350da79b387cfe5c6ef8aeb6e8
Mike Botusov <m.botusov@yandex.ru>
2022-11-29 13:28:12 +0300

    Fix bug with double films

--- fast_api/src/services/transformation.py
+++ fast_api/src/services/transformation.py
@@ -4,17 +4,17 @@ from models.person import PersonDetailed
 
 def person_transformation(person_from_db: PersonDetailed) -> PersonExtended:
     person_roles: set[PersonRole] = set()
-    film_ids: list[str] = []
+    film_ids: set[str] = set()
     for film_id in person_from_db.actor_in:
-        film_ids.append(film_id)
+        film_ids.add(film_id)
         person_roles.add(PersonRole.actor)
 
     for film_id in person_from_db.writer_in:
-        film_ids.append(film_id)
+        film_ids.add(film_id)
         person_roles.add(PersonRole.writer)
 
     for film_id in person_from_db.director_in:
-        film_ids.append(film_id)
+        film_ids.add(film_id)
         person_roles.add(PersonRole.director)
 
     return PersonExtended(id=person_from_db.id, full_name=person_from_db.full_name,
