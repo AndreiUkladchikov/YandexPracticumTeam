79bdd2ddf49e4767efae10071ab4181a98133055
Andrew Ukladchikov <Andrew.Ukladchikov@procoders.com.au>
2022-12-11 19:29:55 +0400

    Fixed redis and refactored films

--- fast_api/src/repository/db_context.py
+++ fast_api/src/repository/db_context.py
@@ -1,34 +1,21 @@
 from __future__ import annotations
 
-import pickle
-import json
+import ast
 from typing import Any
 from aioredis import Redis
 from elasticsearch import AsyncElasticsearch, NotFoundError
-from fastapi import Depends
-from pydantic import parse_obj_as
-
-from core.config import settings
-from db.elastic import get_elastic
-from db.redis import get_redis
 
 
 class DbContext:
-    def __init__(self, redis: Redis, elastic: AsyncElasticsearch, index: str, expire: int, type: type):
+    def __init__(self, redis: Redis, elastic: AsyncElasticsearch, index: str, expire: int):
         self.redis = redis
         self.elastic = elastic
         self.index = index
         self.expire = expire
-        self.type = type
 
     async def _put_to_cache(self, url: str, object: Any):
         await self.redis.set(
             url, object, expire=self.expire
-        )
-
-    async def _put_list_to_cache(self, url: str, items: list[Any]):
-        await self.redis.set(
-            url, pickle.dumps([p for p in items]), expire=self.expire
         )
 
     async def _get_from_cache(self, url: str) -> Any | None:
@@ -36,7 +23,7 @@ class DbContext:
         if not data:
             return None
 
-        return data
+        return ast.literal_eval(data.decode("utf-8"))
 
     async def _get_from_elastic(self, id: str) -> Any | None:
         try:
@@ -46,23 +33,16 @@ class DbContext:
         return doc
 
     async def _get_list_from_elastic(
-        self, page_number: int, page_size: int
+        self, body: Any, page_number: int, page_size: int
     ) -> list[Any] | None:
         try:
             doc = await self.elastic.search(
-                index=self.index, from_=(page_number - 1) * page_size, size=page_size
+                index=self.index, body=body, from_=(page_number - 1) * page_size, size=page_size
             )
 
         except NotFoundError:
             return None
         return doc
-
-    async def _get_list_from_cache(self, url: str) -> list[Any] | None:
-        data = await self.redis.get(url)
-        if not data:
-            return None
-
-        return data
 
     async def get_by_id(self, url: str, id: str) -> Any | None:
         item = await self._get_from_cache(url)
@@ -71,16 +51,17 @@ class DbContext:
             if not item:
                 return None
 
-            # await self._put_to_cache(url, item)
+            await self._put_to_cache(url, bytes(str(item), "utf-8"))
 
         return item
 
-    async def get_list(self, url: str, page_number, page_size) -> list[Any] | None:
-        items = await self._get_list_from_cache(url)
+    async def get_list(self, url: str, page_number, page_size, body: Any) -> list[Any] | None:
+        items = await self._get_from_cache(url)
         if not items:
-            items = await self._get_list_from_elastic(page_number, page_size)
+            items = await self._get_list_from_elastic(body, page_number, page_size)
             if not items:
                 return None
-            # await self._put_list_to_cache(url, items)
+
+            await self._put_to_cache(url, bytes(str(items), "utf-8"))
 
         return items
--- fast_api/src/services/genre.py
+++ fast_api/src/services/genre.py
@@ -1,11 +1,8 @@
 from __future__ import annotations
 
-import json
-import pickle
 from aioredis import Redis
-from elasticsearch import AsyncElasticsearch, NotFoundError
+from elasticsearch import AsyncElasticsearch
 from fastapi import Depends
-from pydantic import parse_obj_as
 
 from core.config import settings
 from db.elastic import get_elastic
@@ -18,14 +15,20 @@ GENRE_CACHE_EXPIRE_IN_SECONDS = settings.cache_expire_in_seconds
 
 class GenreService:
     def __init__(self, redis: Redis, elastic: AsyncElasticsearch):
-        self.db_context = DbContext(redis, elastic, "genres", settings.cache_expire_in_seconds, type(Genre))
+        self.db_context = DbContext(redis, elastic, "genres", settings.cache_expire_in_seconds)
 
     async def get_list_genres(self, url: str, page_number, page_size) -> list[Genre] | None:
         doc = await self.db_context.get_list(url, page_number, page_size)
-        return [Genre(**genre["_source"]) for genre in doc.body["hits"]["hits"]]
+        if not doc:
+            return None
+
+        return [Genre(**genre["_source"]) for genre in doc["hits"]["hits"]]
 
     async def get_genre_by_id(self, url: str, genre_id: str) -> Genre | None:
         data = await self.db_context.get_by_id(url, genre_id)
+        if not data:
+            return None
+
         genre = Genre(**data["_source"])
         return genre
 
