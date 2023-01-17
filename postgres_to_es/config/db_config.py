2a6a0cccf9e0dc12e2800d4803077ea30bfffac9
Mike Botusov <m.botusov@yandex.ru>
2023-01-17 15:06:37 +0300

    change env and config

--- postgres_to_es/config/db_config.py
+++ postgres_to_es/config/db_config.py
@@ -4,7 +4,7 @@ from pydantic import BaseSettings, Field
 # For local debug
 # from dotenv import load_dotenv
 # load_dotenv()
-
+base_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 STATE_CON = os.environ.get('STATE_CON')
 
@@ -12,11 +12,11 @@ ELASTIC_CON = os.environ.get('ELASTIC_CON')
 
 
 class PostgresSettings(BaseSettings):
-    dbname: str = Field(default=os.environ.get('DB_NAME'))
-    user: str = Field(default=os.environ.get('POSTGRES_USER'))
-    password: str = Field(default=os.environ.get('POSTGRES_PASSWORD'))
-    host: str = Field(default=os.environ.get('DB_HOST'))
-    port: str = Field(default=os.environ.get('DB_PORT'))
+    dbname: str = Field(env="DB_NAME")
+    user: str = Field(env="POSTGRES_USER")
+    password: str = Field(env="POSTGRES_PASSWORD")
+    host: str = Field(env="DB_HOST")
+    port: str = Field(env="DB_PORT")
 
     class Config:
-        env_file = '.env'
+        env_file = f"{base_dir}/.env"
