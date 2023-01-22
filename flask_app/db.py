81de96ab544cc08602a6506a8b8cbc661314b0aa
Andrew Ukladchikov <Andrew.Ukladchikov@procoders.com.au>
2023-01-22 13:43:27 +0400

    Fixes 1,5,8

--- flask_app/db.py
+++ flask_app/db.py
@@ -3,12 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 from config import settings
 
-db = SQLAlchemy()
-
 
 def init_db(app: Flask):
     app.config["SQLALCHEMY_DATABASE_URI"] = (
         f"postgresql://{settings.auth_db_username}:"
         f"{settings.auth_db_password}@{settings.auth_db_host}:{settings.auth_db_port}/{settings.auth_db_name}"
     )
-    db.init_app(app)
+    SQLAlchemy().init_app(app)
