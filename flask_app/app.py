90142427671a4e20901f60d90a03cf5808b1b187
Andrei Ukladchikov <andrei.ukladchikov@auriga.com>
2023-01-28 21:46:16 +0400

    Deleted comments

--- flask_app/app.py
+++ flask_app/app.py
@@ -15,7 +15,6 @@ from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
-# from opentelemetry.exporter.jaeger.thrift import JaegerExporter
 
 import constants
 import jaeger
@@ -36,14 +35,6 @@ from v1.roles.roles import roles_blueprint
 
 def configure_tracer() -> None:
     trace.set_tracer_provider(TracerProvider())
-    # trace.get_tracer_provider().add_span_processor(
-    #     BatchSpanProcessor(
-    #         JaegerExporter(
-    #             agent_host_name='localhost',
-    #             agent_port=6831,
-    #         )
-    #     )
-    # )
     trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(ConsoleSpanExporter()))
 
 
