6197d9054dd8c31ef7b52380bb7e73c2081df6f7
Mike Botusov <m.botusov@yandex.ru>
2023-02-13 19:16:01 +0300

    Replace count status -> http.status

--- ugc_backend/tests/functional/test_ugc_service.py
+++ ugc_backend/tests/functional/test_ugc_service.py
@@ -1,3 +1,4 @@
+import http
 import json
 
 import requests
@@ -9,24 +10,22 @@ from tests.functional.constants import (
 )
 
 headers = {"Content-type": "application/json", "Accept": "text/plain"}
-
-
 url_to_ugc = f"http://{settings.ugc_backend_host}:{settings.ugc_backend_port}/views"
 
 
 class TestUGCService:
     def test_data(self):
         res = requests.post(url_to_ugc, headers=headers, data=json.dumps(test_data))
-        assert res.status_code == 200
+        assert res.status_code == http.HTTPStatus.OK
 
     def test_invalid_user_id(self):
         res = requests.post(
             url_to_ugc, headers=headers, data=json.dumps(test_topic_name)
         )
-        assert res.status_code == 422
+        assert res.status_code == http.HTTPStatus.UNPROCESSABLE_ENTITY
 
     def test_invalid_timestamp(self):
         res = requests.post(
             url_to_ugc, headers=headers, data=json.dumps(test_invalid_timestamp)
         )
-        assert res.status_code == 422
+        assert res.status_code == http.HTTPStatus.UNPROCESSABLE_ENTITY
