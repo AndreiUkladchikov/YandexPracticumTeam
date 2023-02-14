75a2bcaf0b2523fc26bb9ec9457d23573e14b271
Mike Botusov <61203959+mikeGot@users.noreply.github.com>
2023-02-14 16:08:44 +0300

    Sprint 8 fix bugs (#116)
    * Replace count status -> http.status
    
    * Delete mkdir (#3)
    
    * Add Depends to router

--- ugc_backend/main.py
+++ ugc_backend/main.py
@@ -1,11 +1,33 @@
 import uvicorn
+from aiokafka import AIOKafkaProducer, errors
 from fastapi import FastAPI
-from src.aggregate_to_kafka import router
+from src.aggregate_to_kafka import dependency, router
 from src.config import settings
+from src.custom_log import logger
 
 app = FastAPI(
     title="ugc_backend", docs_url="/api/openapi", openapi_url="/api/openapi.json"
 )
+
+
+@app.on_event("startup")
+async def startup_event():
+    try:
+        dependency.kafka_producer = AIOKafkaProducer(
+            bootstrap_servers=f"{settings.kafka_host}:{settings.kafka_port}"
+        )
+        await dependency.kafka_producer.start()
+    except errors.KafkaConnectionError as kafka_error:
+        logger.critical(kafka_error)
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    try:
+        await dependency.kafka_producer.stop()
+    except errors.KafkaConnectionError as kafka_error:
+        logger.critical(kafka_error)
+        raise kafka_error
 
 
 app.include_router(router.router, prefix="/views", tags=["views"])
