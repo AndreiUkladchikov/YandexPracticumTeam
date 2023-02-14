59e9d17d9b933135965506896de9e543330a8793
r0steg <r.sulitskiy@gmail.com>
2023-02-14 21:45:52 +0300

    Remove RedisStorage, update enviroment and docker compose

--- ugc_etl/etl/storage/storage.py
+++ ugc_etl/etl/storage/storage.py
@@ -23,48 +23,6 @@ class BaseStorage(abc.ABC):
         pass
 
 
-class RedisStorage(BaseStorage):
-    """Собирает и хранит батч для загрузки в ClickHouse."""
-
-    def __init__(self,
-                 host: str = settings.redis_storage.host,
-                 port: int = settings.redis_storage.port,
-                 list_key: str = settings.redis_list_key) -> None:
-        """Создаем подключение к Redis, получаем ключ списка.
-
-        Args:
-            host (str, optional): Redis host.
-            port (int, optional): Redis port.
-            list_key (str, optional): Key of list in Redis.
-        """
-        self.redis = redis.StrictRedis(
-            host=host,
-            port=port,
-            charset="utf-8",
-            decode_responses=True,
-        )
-        self.list_key = list_key
-
-    def save(self, value: str) -> None:
-        """Добавляем значение в список Redis.
-
-        Args:
-            value (str): значение для добавления в список.
-        """
-        self.redis.rpush(self.list_key, value)
-
-    def retrieve(
-        self,
-    ) -> Iterator[str, ]:
-        """Читаем все значения из списка Redis."""
-        for _ in range(self.redis.llen(self.list_key)):
-            yield self.redis.lpop(self.list_key)
-
-    def current_batch_size(self) -> int:
-        """Возращает размер батча в Redis."""
-        return self.redis.llen(self.list_key)
-
-
 class ListStorage(BaseStorage):
 
     def __init__(self) -> None:
