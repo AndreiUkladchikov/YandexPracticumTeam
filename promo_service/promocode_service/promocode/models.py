39900e67c8dbc6c6e4e57aa108863523dcc3d843
r0steg <r.sulitskiy@gmail.com>
2023-03-30 22:39:58 +0300

    Add model translations

--- promo_service/promocode_service/promocode/models.py
+++ promo_service/promocode_service/promocode/models.py
@@ -20,11 +20,12 @@ def get_promo_code(num_chars=8) -> str:
 
 
 class PromocodeType(models.Model):
-    type_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    description = models.TextField()
-    discount = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)])
-    duration = models.PositiveIntegerField()
-    max_number_activation = models.PositiveIntegerField(default=1)
+    type_id = models.UUIDField(_('type id'), primary_key=True, default=uuid.uuid4, editable=False, )
+    description = models.TextField(_('description'), )
+    discount = models.IntegerField(_('discount'), validators=[MinValueValidator(1), MaxValueValidator(100)], )
+    duration = models.PositiveIntegerField(_('duration'), )
+    # ? Давайте если ноль, то кол-во активаций будет не ограничено?
+    max_number_activation = models.PositiveIntegerField(_('max number activation'), default=0, )
 
     def __str__(self):
         return self.description
@@ -36,13 +37,15 @@ class PromocodeType(models.Model):
 
 
 class Promocode(models.Model):
-    promo_id = models.UUIDField(_("id"), primary_key=True, default=uuid.uuid4, editable=False)
-    promo_value = models.CharField(_("promo value"), max_length=20,
-                                   validators=[MinLengthValidator(8)], default=get_promo_code)
-    created_at = models.DateTimeField(_("created at"), auto_now_add=True)
+    promo_id = models.UUIDField(_("id"), primary_key=True, default=uuid.uuid4, editable=False, )
+    promo_value = models.CharField(_("promo value"),
+                                   max_length=20,
+                                   validators=[MinLengthValidator(8)],
+                                   default=get_promo_code, )
+    created_at = models.DateTimeField(_("created at"), auto_now_add=True, )
     is_valid = models.BooleanField(_("is valid"), )
-    number_activations = models.PositiveIntegerField(default=0, editable=False)
-    personal_user_id = models.UUIDField(blank=True, null=True)
+    number_activations = models.PositiveIntegerField(_("number activations"), default=0, editable=False, )
+    personal_user_id = models.UUIDField(_('personal user id'), blank=True, null=True, )
     activate_until = models.DateTimeField(_("activate until"), )
 
     type_of_promocode = models.ForeignKey(PromocodeType,
@@ -56,10 +59,13 @@ class Promocode(models.Model):
 
 
 class PromocodeUserHistory(models.Model):
-    promo_id = models.ForeignKey(Promocode, on_delete=models.CASCADE)
-    user_id = models.UUIDField()
-    activated_at = models.DateTimeField(auto_now_add=True)  # при создании записи мы сразу активируем промокод
-    expire_at = models.DateTimeField()
+    promo_id = models.ForeignKey(Promocode,
+                                 on_delete=models.CASCADE,
+                                 verbose_name=_('promo id'), )
+    user_id = models.UUIDField(_('user id'), )
+    # при создании записи мы сразу активируем промокод
+    activated_at = models.DateTimeField(_('activated at'), auto_now_add=True, )
+    expire_at = models.DateTimeField(_('expire at'), )
 
     class Meta:
         db_table = "promocode_user_history"
