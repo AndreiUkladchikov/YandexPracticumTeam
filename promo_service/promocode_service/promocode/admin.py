26f3551f3b3f44bd59fae8a28a2659620aa52d7c
r0steg <r.sulitskiy@gmail.com>
2023-03-30 22:53:26 +0300

    Update admin views

--- promo_service/promocode_service/promocode/admin.py
+++ promo_service/promocode_service/promocode/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from import_export.admin import ExportMixin
-from rangefilter.filters import DateTimeRangeFilter
+from rangefilter.filters import DateTimeRangeFilter, NumericRangeFilter
 
 from .models import PromocodeUserHistory, Promocode, PromocodeType
 
@@ -28,5 +28,41 @@ class PromocodeAdmin(ExportMixin, admin.ModelAdmin):
     list_per_page = 50
 
 
-admin.site.register(PromocodeUserHistory)
-admin.site.register(PromocodeType)
+@admin.register(PromocodeUserHistory)
+class PromocodeUserHistoryAdmin(ExportMixin, admin.ModelAdmin):
+
+    ordering = ('-activated_at', )
+    list_display = ('promo_id',
+                    'user_id',
+                    'activated_at',
+                    'expire_at', )
+    list_filter = (('activated_at', DateTimeRangeFilter),
+                   ('expire_at', DateTimeRangeFilter), )
+    search_fields = ('user_id',
+                     'promo_id__type_of_promocode__description',
+                     'promo_id__promo_value', )
+    list_display_links = ('promo_id',
+                          'user_id', )
+    raw_id_fields = ('promo_id', )
+
+    list_per_page = 50
+
+
+@admin.register(PromocodeType)
+class PromocodeTypeAdmin(ExportMixin, admin.ModelAdmin):
+
+    ordering = ('description', )
+    list_display = ('description',
+                    'discount',
+                    'duration',
+                    'max_number_activation', )
+    list_filter = (('discount', NumericRangeFilter),
+                   ('duration', NumericRangeFilter),
+                   ('max_number_activation', NumericRangeFilter), )
+    search_fields = ('description',
+                     'discount',
+                     'duration',
+                     'max_number_activation', )
+    list_display_links = ('description', )
+
+    list_per_page = 50
