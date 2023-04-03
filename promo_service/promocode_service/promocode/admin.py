9b2aa8b9e9562b3cf565ffcc55d95992e1b4910a
rsulitskiy <r.sulitskiy@gmail.com>
2023-04-03 23:55:12 +0300

    Update admin views and translations

--- promo_service/promocode_service/promocode/admin.py
+++ promo_service/promocode_service/promocode/admin.py
@@ -2,28 +2,28 @@ from django.contrib import admin
 from import_export.admin import ExportMixin
 from rangefilter.filters import DateTimeRangeFilter, NumericRangeFilter
 
-from .models import Promocode, PromocodeType, PromocodeUserHistory
+from .models import Promocode, PromocodeType, PromocodeUserHistory, Task
 
 
 @admin.register(Promocode)
 class PromocodeAdmin(ExportMixin, admin.ModelAdmin):
 
-    ordering = ('-created_at', )
-    list_display = ('promo_value',
-                    'promocode_type_id',
-                    'is_valid',
-                    'activate_until',
-                    'created_at', )
-    list_filter = ('is_valid',
-                   ('promocode_type_id', admin.RelatedOnlyFieldListFilter),
-                   ('created_at', DateTimeRangeFilter),
-                   ('activate_until', DateTimeRangeFilter), )
-    search_fields = ('promocode_type_id__description',
-                     'promocode_type_id__discount',
-                     'promocode_type_id__duration',
-                     'promo_value', )
-    list_display_links = ('promo_value', )
-    raw_id_fields = ('promocode_type_id', )
+    ordering = ("-created_at", )
+    list_display = ("promo_value",
+                    "promocode_type_id",
+                    "is_valid",
+                    "activate_until",
+                    "created_at", )
+    list_filter = ("is_valid",
+                   ("promocode_type_id", admin.RelatedOnlyFieldListFilter),
+                   ("created_at", DateTimeRangeFilter),
+                   ("activate_until", DateTimeRangeFilter), )
+    search_fields = ("promocode_type_id__description",
+                     "promocode_type_id__discount",
+                     "promocode_type_id__duration",
+                     "promo_value", )
+    list_display_links = ("promo_value", )
+    raw_id_fields = ("promocode_type_id", )
 
     list_per_page = 50
 
@@ -31,19 +31,17 @@ class PromocodeAdmin(ExportMixin, admin.ModelAdmin):
 @admin.register(PromocodeUserHistory)
 class PromocodeUserHistoryAdmin(ExportMixin, admin.ModelAdmin):
 
-    ordering = ('-activated_at', )
-    list_display = ('promocode_id',
-                    'user_id',
-                    'activated_at',
-                    'expire_at', )
-    list_filter = (('activated_at', DateTimeRangeFilter),
-                   ('expire_at', DateTimeRangeFilter), )
-    search_fields = ('user_id',
-                     'promocode_id__promocode_type_id__description',
-                     'promocode_id__promo_value', )
-    list_display_links = ('promocode_id',
-                          'user_id', )
-    raw_id_fields = ('promocode_id', )
+    ordering = ("-activated_at", )
+    list_display = ("promocode_id",
+                    "user_id",
+                    "activated_at", )
+    list_filter = (("activated_at", DateTimeRangeFilter), )
+    search_fields = ("user_id",
+                     "promocode_id__promocode_type_id__description",
+                     "promocode_id__promo_value", )
+    list_display_links = ("promocode_id",
+                          "user_id", )
+    raw_id_fields = ("promocode_id", )
 
     list_per_page = 50
 
@@ -51,18 +49,37 @@ class PromocodeUserHistoryAdmin(ExportMixin, admin.ModelAdmin):
 @admin.register(PromocodeType)
 class PromocodeTypeAdmin(ExportMixin, admin.ModelAdmin):
 
-    ordering = ('description', )
-    list_display = ('description',
-                    'discount',
-                    'duration',
-                    'max_number_activation', )
-    list_filter = (('discount', NumericRangeFilter),
-                   ('duration', NumericRangeFilter),
-                   ('max_number_activation', NumericRangeFilter), )
-    search_fields = ('description',
-                     'discount',
-                     'duration',
-                     'max_number_activation', )
-    list_display_links = ('description', )
+    ordering = ("description", )
+    list_display = ("description",
+                    "discount",
+                    "duration",
+                    "max_number_activation", )
+    list_filter = (("discount", NumericRangeFilter),
+                   ("duration", NumericRangeFilter),
+                   ("max_number_activation", NumericRangeFilter), )
+    search_fields = ("description",
+                     "discount",
+                     "duration",
+                     "max_number_activation", )
+    list_display_links = ("description", )
 
     list_per_page = 50
+
+
+@admin.register(Task)
+class TaskAdmin(ExportMixin, admin.ModelAdmin):
+
+    ordering = ("description", )
+    list_display = ("description",
+                    "api_endpoint",
+                    "is_complete",
+                    "promocode_type", )
+    list_filter = ("is_complete",
+                   ("created_at", DateTimeRangeFilter), )
+    search_fields = ("description",
+                     "api_endpoint",
+                     "promocode_type__description", )
+    list_display_links = ("description", )
+    raw_id_fields = ("promocode_type", )
+
+    list_per_page = 25
--- promo_service/promocode_service/promocode/models.py
+++ promo_service/promocode_service/promocode/models.py
@@ -14,8 +14,8 @@ def get_promo_code(num_chars=8) -> str:
 
     :return: promo code
     """
-    code_chars = '123456789ABCDEFGHIJKLMNPQRSTUVWXYZ'
-    code = ''
+    code_chars = "123456789ABCDEFGHIJKLMNPQRSTUVWXYZ"
+    code = ""
     for i in range(0, num_chars):
         slice_start = random.randint(0, len(code_chars) - 1)
         code += code_chars[slice_start: slice_start + 1]
@@ -33,19 +33,19 @@ class PromocodeType(models.Model):
         max_number_activation: int максимальное кол-во раз, которое пользователь
                                    может воспользоваться промоколом (если 0 то неограничено)
     """
-    id = models.UUIDField(_('type id'), primary_key=True, default=uuid.uuid4, editable=False, )
-    description = models.TextField(_('description'), )
-    discount = models.IntegerField(_('discount'), validators=[MinValueValidator(1), MaxValueValidator(100)], )
-    duration = models.PositiveIntegerField(_('duration'), )
-    max_number_activation = models.PositiveIntegerField(_('max number activation'), default=0, )
+    id = models.UUIDField(_("type id"), primary_key=True, default=uuid.uuid4, editable=False, )
+    description = models.TextField(_("description"), )
+    discount = models.IntegerField(_("discount"), validators=[MinValueValidator(1), MaxValueValidator(100)], )
+    duration = models.PositiveIntegerField(_("duration"), )
+    max_number_activation = models.PositiveIntegerField(_("max number activation"), default=0, )
 
     def __str__(self):
         return self.description
 
     class Meta:
         db_table = "promocode_type"
-        verbose_name = _('promocode type')
-        verbose_name_plural = _('promocode type')
+        verbose_name = _("promocode type")
+        verbose_name_plural = _("promocode type")
 
 
 class Task(models.Model):
@@ -54,30 +54,32 @@ class Task(models.Model):
     Prop:
         id: uuid идентификатор задания
         description: str краткое описание задания
+        created_at: datetime метка времени, когда задание было создано
         api_endpoint: str url-адрес ручки сервиса, которая вернет перечень id пользователей,
                           для которых необходимо создать промокоды (например, можно из сервиса
                           статистики получить сто самых активных комментаторов или ревьюеров фильмов)
         is_complete: bool текущий статус задания (True если задание выполнено и созданы промокоды)
-        promocode_type_id: uuid внешний ключ указывающий на тип промокода
+        promocode_type: uuid внешний ключ указывающий на тип промокода
                                 (размер скидки, период действия в днях,
                                 кол-во активаций для пользователя)
     """
-    id = models.UUIDField(_('type id'), primary_key=True, default=uuid.uuid4, editable=False, )
-    description = models.TextField(_('description'), )
-    api_endpoint = models.URLField(_('api endpoint'), )
-    is_complete = models.BooleanField(_('is complete'), default=False, )
+    id = models.UUIDField(_("type id"), primary_key=True, default=uuid.uuid4, editable=False, )
+    description = models.TextField(_("description"), )
+    created_at = models.DateTimeField(_("created at"), auto_now_add=True, )
+    api_endpoint = models.URLField(_("api endpoint"), )
+    is_complete = models.BooleanField(_("is complete"), default=False, editable=False, )
 
-    promocode_type_id = models.ForeignKey(PromocodeType,
-                                          on_delete=models.CASCADE,
-                                          verbose_name=_("type of promocode"), )
+    promocode_type = models.ForeignKey(PromocodeType,
+                                       on_delete=models.CASCADE,
+                                       verbose_name=_("type of promocode"), )
 
     def __str__(self) -> str:
         return self.description
 
     class Meta:
         db_table = "task"
-        verbose_name = _('task')
-        verbose_name_plural = _('task')
+        verbose_name = _("task")
+        verbose_name_plural = _("tasks")
 
 
 class Promocode(models.Model):
@@ -101,7 +103,7 @@ class Promocode(models.Model):
     created_at = models.DateTimeField(_("created at"), auto_now_add=True, )
     is_valid = models.BooleanField(_("is valid"), )
     number_activations = models.PositiveIntegerField(_("number activations"), default=0, editable=False, )
-    personal_user_id = models.UUIDField(_('personal user id'), blank=True, null=True, )
+    personal_user_id = models.UUIDField(_("personal user id"), blank=True, null=True, )
     activate_until = models.DateTimeField(_("activate until"), )
 
     promocode_type_id = models.ForeignKey(PromocodeType,
@@ -113,8 +115,8 @@ class Promocode(models.Model):
 
     class Meta:
         db_table = "promocode"
-        verbose_name = _('promocode')
-        verbose_name_plural = _('promocodes')
+        verbose_name = _("promocode")
+        verbose_name_plural = _("promocodes")
 
 
 class PromocodeUserHistory(models.Model):
@@ -128,14 +130,14 @@ class PromocodeUserHistory(models.Model):
     """
     promocode_id = models.ForeignKey(Promocode,
                                      on_delete=models.CASCADE,
-                                     verbose_name=_('promo id'), )
-    user_id = models.UUIDField(_('user id'), )
-    activated_at = models.DateTimeField(_('activated at'), auto_now_add=True, )
+                                     verbose_name=_("promo id"), )
+    user_id = models.UUIDField(_("user id"), )
+    activated_at = models.DateTimeField(_("activated at"), auto_now_add=True, )
 
     class Meta:
         db_table = "promocode_user_history"
-        verbose_name = _('promocode history')
-        verbose_name_plural = _('promocode history')
+        verbose_name = _("promocode history")
+        verbose_name_plural = _("promocode history")
         constraints = [
-            models.UniqueConstraint(fields=['promocode_id', 'user_id'], name='unique pair promocode-user')
+            models.UniqueConstraint(fields=["promocode_id", "user_id"], name="unique pair promocode-user")
         ]
