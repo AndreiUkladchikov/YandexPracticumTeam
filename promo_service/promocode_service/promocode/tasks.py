c10706ce2e1be43ad1909beec94c3eb3c010b10b
rsulitskiy <r.sulitskiy@gmail.com>
2023-04-04 13:15:14 +0300

    Add test for celery base build and production compose

--- promo_service/promocode_service/promocode/tasks.py
+++ promo_service/promocode_service/promocode/tasks.py
@@ -5,5 +5,7 @@ from celery import shared_task
 
 @shared_task
 def create_promocodes(task_type):
+    """Тестовое задание, чтобы проверить celery.
+    """
     time.sleep(int(task_type) * 10)
     return True
--- promo_service/promocode_service/promocode/tests.py
+++ promo_service/promocode_service/promocode/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .tasks import create_promocodes
+
+
+class TestTasks(TestCase):
+
+    def test_create_promocodes_task(self):
+        """Тестируем корректность настройки celery
+        """
+        assert create_promocodes.run(1)
+        assert create_promocodes.run(2)
+        assert create_promocodes.run(3)
