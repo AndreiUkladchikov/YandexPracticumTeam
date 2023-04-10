9d9e19e28d294b9eb761a04859689898f72b0c02
r0steg <r.sulitskiy@gmail.com>
2023-04-10 21:32:00 +0300

    Move create promocode task to celery task

--- promo_service/promocode_service/promocode/signals.py
+++ promo_service/promocode_service/promocode/signals.py
@@ -1,14 +1,8 @@
-from datetime import timedelta
-from uuid import UUID, uuid4
-
-import requests
-from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.utils import timezone
 
-from .models import Task, Promocode, PromocodeType
-from .tasks import notify_user
+from .models import Task
+from .tasks import create_promocodes_task
 
 
 @receiver(post_save, sender=Task)
@@ -17,37 +11,4 @@ def generate_promocodes(sender, instance: Task, created: bool, **kwargs: dict) -
     и уведомляем пользователей о новом промокоде по notify_api_endpoint.
     """
     if created:
-        users = get_users(instance.users_api_endpoint)
-
-        for user_id in users:
-            promocode = create_promocode(user_id, instance.promocode_type)
-            notify_user.delay(user_id,
-                              promocode.promo_value,
-                              instance.notify_api_endpoint)
-
-        instance.is_complete = True
-        instance.save()
-
-
-def get_users(users_api_endpoint: str) -> list[str | UUID]:
-    """Получаем по указаному адресу список идентификаторов пользователей 
-    для которых необходимо сгенерировать персональные промокоды.
-    """
-    if not settings.DEBUG:
-        resp = requests.get(users_api_endpoint)
-        users = resp.json().get("users", [])
-    else:
-        users = [uuid4() for _ in range(100)]
-    return users
-
-
-def create_promocode(user_id: UUID, promocode_type: PromocodeType) -> Promocode:
-    """Создаем новый персональный промокод для пользователя.
-    """
-    new_promocode = Promocode.objects.create(
-        is_valid=True,
-        personal_user_id=user_id,
-        activate_until=timezone.now() + timedelta(days=promocode_type.duration),
-        promocode_type=promocode_type,
-    )
-    return new_promocode
+        create_promocodes_task.delay(instance.id)
--- promo_service/promocode_service/promocode/tasks.py
+++ promo_service/promocode_service/promocode/tasks.py
@@ -1,8 +1,12 @@
-from uuid import UUID
+from datetime import timedelta
+from uuid import UUID, uuid4
 
 import requests
 from django.conf import settings
+from django.utils import timezone
 from celery import shared_task
+
+from promocode.models import Task, Promocode, PromocodeType
 
 
 @shared_task(
@@ -24,3 +28,46 @@ def notify_user(self, user_id: UUID, promocode_value: str, notify_api_endpoint: 
         _ = requests.post(notify_api_endpoint, json=payload)
     else:
         pass
+
+
+@shared_task
+def create_promocodes_task(task_id: UUID) -> None:
+    """Создаем промокоды для пользователей полученных по указанному users_api_endpoint
+    и уведомляем пользователей о новом промокоде по notify_api_endpoint.
+    """
+    task = Task.objects.get(id=task_id)
+
+    users = get_users(task.users_api_endpoint)
+
+    for user_id in users:
+        promocode = create_promocode(user_id, task.promocode_type)
+        notify_user.delay(user_id,
+                          promocode.promo_value,
+                          task.notify_api_endpoint)
+
+    task.is_complete = True
+    task.save()
+
+
+def get_users(users_api_endpoint: str) -> list[str | UUID]:
+    """Получаем по указаному адресу список идентификаторов пользователей 
+    для которых необходимо сгенерировать персональные промокоды.
+    """
+    if not settings.DEBUG:
+        resp = requests.get(users_api_endpoint)
+        users = resp.json().get("users", [])
+    else:
+        users = [uuid4() for _ in range(100)]
+    return users
+
+
+def create_promocode(user_id: UUID, promocode_type: PromocodeType) -> Promocode:
+    """Создаем новый персональный промокод для пользователя.
+    """
+    new_promocode = Promocode.objects.create(
+        is_valid=True,
+        personal_user_id=user_id,
+        activate_until=timezone.now() + timedelta(days=promocode_type.duration),
+        promocode_type=promocode_type,
+    )
+    return new_promocode
